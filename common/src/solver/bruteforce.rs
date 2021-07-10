f27118ef0af1f612ce2f561bd3301c3e7095778a
Gleb Golubitsky <sectoid@gnolltech.org>
2021-07-10 22:00:08 +0300

    bruteforce: seems working now
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- common/src/solver/bruteforce.rs
+++ common/src/solver/bruteforce.rs
@@ -24,19 +24,36 @@ impl BruteforceSolver {
         // the last vertex left. brute-forcing...
         let mut new_pose = None;
         let mut best_score = last_best;
-        for next_y in start.1..self.solver.field_max.1 {
+        let mut next_y = start.1;
+        let mut next_x = start.0;
+        while next_y <= self.solver.field_max.1 {
             if vert_idx < 2 {
-                log::debug!("Starting Y-step {} for idx: {}...", next_y, vert_idx);
+                // log::debug!("Starting Y-step {} for idx: {}...", next_y, vert_idx);
             }
-            'loop_x: for next_x in start.0..self.solver.field_max.0 {
+            'loop_x: while next_x <= self.solver.field_max.0 {
+
+                if vert_idx < 2 {
+                    // log::debug!("Starting X-step {} for idx: {}...", next_x, vert_idx);
+                }
+                if next_y > self.solver.field_max.1 {
+                    break;
+                }
+
                 let vertice = problem::Point(next_x, next_y);
+                // log::debug!("checking {} vertice: {:?}, vertices: {:?}, is_hole: {}", vert_idx, vertice, vertices, self.solver.is_hole(&vertice));
+
+                next_x += 1;
+                if next_x > self.solver.field_max.0 {
+                    next_x = 0;
+                    next_y += 1;
+                }
+
                 if !self.solver.is_hole(&vertice) {
                     continue;
                 }
 
                 for idx in 0..vert_idx {
                     let edge_distance = distances[vert_idx*vertices.len()+idx];
-                    // log::debug!("vert_idx: {}, idx: {}, dmin: {}, dmax: {}", vert_idx, idx, dmin, dmax);
                     if edge_distance == -1 {
                         continue;
                     }
@@ -50,9 +67,17 @@ impl BruteforceSolver {
                 vertices[vert_idx] = vertice;
 
                 if vert_idx == vertices.len() - 1 {
+                    // log::debug!("scoring candidate... {:?}", vertices);
+
                     match self.solver.problem.score_vertices(vertices) {
                         Ok(score) => {
                             log::debug!("Found solution with score {:?}: {:?}", score, vertices);
+                            if score == 0 { // perfect solution found
+                                return (0, Some(problem::Pose {
+                                    vertices: vertices.clone(),
+                                    bonuses: None, // fixme
+                                }))
+                            }
                             if score < best_score {
                                 best_score = score;
                                 new_pose = Some(problem::Pose {
@@ -65,8 +90,11 @@ impl BruteforceSolver {
                     }
                 }
                 else {
-                    let (rec_best_score, rec_new_pose) = self.run(vertice, vert_idx + 1, best_score, vertices,
+                    let (rec_best_score, rec_new_pose) = self.run(self.solver.field_min, vert_idx + 1, best_score, vertices,
                                                                   distances);
+                    if rec_best_score == 0 {
+                        return (0, rec_new_pose);
+                    }
                     if rec_best_score < best_score {
                         best_score = rec_best_score;
                         new_pose = rec_new_pose;
@@ -74,7 +102,7 @@ impl BruteforceSolver {
                 }
             }
             if vert_idx < 2 {
-                log::debug!("Passed Y-step {} for idx: {}", next_y, vert_idx);
+                // log::debug!("Passed Y-step {} for idx: {}", next_y, vert_idx);
             }
 
         }
@@ -83,7 +111,6 @@ impl BruteforceSolver {
     }
 
     pub fn solve(&self) -> Option<problem::Pose> {
-        let start = self.solver.field_min;
         let mut vertices = self.solver.problem.figure.vertices.clone();
         let mut distances = vec![-1; vertices.len() * vertices.len()];
 
@@ -94,7 +121,7 @@ impl BruteforceSolver {
         }
 
         log::debug!("distance matrix: {:?}", distances);
-        let (score, pose) = self.run(start, 0, i64::MAX, &mut vertices, &distances);
+        let (score, pose) = self.run(self.solver.field_min, 0, i64::MAX, &mut vertices, &distances);
         println!("Found solution with score {:?}: {:?}", score, pose);
         pose
     }
