708506e218e3216d6857c02a05c54f78264ee6f4
Alexey Voznyuk <me@swizard.info>
2021-07-10 16:57:03 +0300

    params and temperature

--- common/src/solver/simulated_annealing.rs
+++ common/src/solver/simulated_annealing.rs
@@ -5,22 +5,34 @@ use crate::{
     problem,
 };
 
+#[derive(Clone, Copy, Debug)]
+pub struct Params {
+    pub max_temp: f64,
+    pub cooling_step_temp: f64,
+}
+
 pub struct SimulatedAnnealingSolver {
     solver: solver::Solver,
+    params: Params,
     vertices: Vec<problem::Point>,
+    temp: f64,
 }
 
 impl SimulatedAnnealingSolver {
-    pub fn new(solver: solver::Solver) -> SimulatedAnnealingSolver {
+    pub fn new(solver: solver::Solver, params: Params) -> SimulatedAnnealingSolver {
         let vertices = generate_init_vertices(&solver);
+        let temp = params.max_temp;
         SimulatedAnnealingSolver {
             solver,
+            params,
             vertices,
+            temp,
         }
     }
 
     pub fn reset(&mut self) {
         self.vertices = generate_init_vertices(&self.solver);
+        self.temp = self.params.max_temp;
     }
 
     pub fn vertices(&self) -> &[problem::Point] {
