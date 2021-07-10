4e1e3641a256a5ade1f126d6781154a79ba9ba31
Gleb Golubitsky <sectoid@gnolltech.org>
2021-07-10 17:45:16 +0300

    bruteforce: untested solver
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- common/src/solver/bruteforce.rs
+++ common/src/solver/bruteforce.rs
@@ -15,7 +15,55 @@ impl BruteforceSolver {
         }
     }
 
-    pub fn solve(self) -> Option<problem::Pose> {
-        None
+
+    fn run(&self,
+           start: problem::Point, vert_idx: usize, last_best: i64,
+           vertices: &mut Vec<problem::Point>) -> (i64, Option<problem::Pose>) {
+
+        // the last vertex left. brute-forcing...
+        let mut new_pose = None;
+        let mut best_score = last_best;
+        for next_y in start.1..self.solver.field_max.1 {
+            for next_x in start.0..self.solver.field_max.0 {
+                let vertice = problem::Point(next_x, next_y);
+                if !self.solver.is_hole(&vertice) {
+                    continue;
+                }
+
+                vertices[vert_idx] = vertice;
+
+                if vert_idx == vertices.len() - 1 {
+                    match self.solver.problem.score_vertices(vertices) {
+                        Ok(score) => {
+                            if score < best_score {
+                                best_score = score;
+                                new_pose = Some(problem::Pose {
+                                    vertices: vertices.clone(),
+                                    bonuses: None, // fixme
+                                })
+                            }
+                        },
+                        _ => continue,
+                    }
+                }
+                else {
+                    let (rec_best_score, rec_new_pose) = self.run(vertice, vert_idx + 1, best_score, vertices);
+                    if rec_best_score < best_score {
+                        best_score = rec_best_score;
+                        new_pose = rec_new_pose;
+                    }
+                }
+            }
+        }
+
+        (best_score, new_pose)
+    }
+
+    pub fn solve(&self) -> Option<problem::Pose> {
+        let start = self.solver.field_min;
+        let mut vertices = self.solver.problem.figure.vertices.clone();
+        let (score, pose) = self.run(start, 0, i64::MAX, &mut vertices);
+        println!("Found solution with score {:?}: {:?}", score, pose);
+        pose
     }
 }
