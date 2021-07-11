0a5d1f477c26c215fef0f20ea89395b51d03e075
Gleb Golubitsky <sectoid@gnolltech.org>
2021-07-11 19:43:01 +0300

    bruteforce-hole: add skipping of hole vertices that break edges
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- common/src/solver/bruteforce_hole.rs
+++ common/src/solver/bruteforce_hole.rs
@@ -10,13 +10,13 @@ use crate::{
 #[allow(dead_code)]
 pub struct BruteforceHoleSolver {
     solver: solver::Solver,
-    step: i64,
-    total_steps: i64,
+    step: u128,
+    total_steps: u128,
 }
 
 impl BruteforceHoleSolver {
     pub fn new(solver: solver::Solver) -> BruteforceHoleSolver {
-        let hole_size = solver.problem.hole.len() as i64;
+        let hole_size = solver.problem.hole.len() as u128;
         let figure_size = solver.problem.figure.vertices.len() as u32;
         let total_steps = hole_size.pow(figure_size);
         BruteforceHoleSolver {
@@ -29,10 +29,17 @@ impl BruteforceHoleSolver {
     pub fn solve(&mut self) -> Option<problem::Pose> {
         let mut vertices = self.solver.problem.figure.vertices.clone();
         let hole = self.solver.problem.hole.clone();
+        let mut distances = vec![-1; vertices.len() * vertices.len()];
+
+        for &problem::Edge(from_idx, to_idx) in self.solver.problem.figure.edges.iter() {
+            let distance = problem::distance(&vertices[from_idx], &vertices[to_idx]);
+            distances[from_idx*vertices.len() + to_idx] = distance;
+            distances[to_idx*vertices.len() + from_idx] = distance;
+        }
 
         println!("Bruteforce of hole size {} for figure size {}. It will take ~{} iterations",
                  hole.len(), vertices.len(), self.total_steps);
-        let (score, pose) = self.run(0, i64::MAX, &mut vertices, &hole);
+        let (score, pose) = self.run(0, i64::MAX, &mut vertices, &hole, &distances);
         match pose {
             None => println!("Found solution not found..."),
             Some(ref pose) => println!("Found solution with score {:?}: {:?}", score, pose),
@@ -43,15 +50,29 @@ impl BruteforceHoleSolver {
     fn run(&mut self, vert_idx: usize,
            last_best_score: i64,
            vertices: &mut Vec<problem::Point>,
-           hole: &Vec<problem::Point>) -> (i64, Option<problem::Pose>) {
+           hole: &Vec<problem::Point>,
+           distances: &[i64]) -> (i64, Option<problem::Pose>) {
 
         let mut best_pose_score = last_best_score;
         let mut best_pose = None;
 
-        for hole_vertice in hole {
+        'next_hole_vertice: for hole_vertice in hole {
+            for idx in 0..vert_idx {
+                let edge_distance = distances[vert_idx*vertices.len()+idx];
+                if edge_distance == -1 {
+                    continue;
+                }
+                let distance = problem::distance(&hole_vertice, &vertices[idx]);
+
+                if ((distance as f64 / edge_distance as f64) - 1_f64).abs() > self.solver.problem.epsilon as f64 / 1000000_f64 {
+                    self.track_progress((hole.len() as u128).pow((vertices.len() - vert_idx) as u32));
+                    continue 'next_hole_vertice;
+                }
+            }
+
             vertices[vert_idx] = *hole_vertice;
             let (new_score, new_pose) = if vert_idx == vertices.len() - 1 {
-                self.track_progress();
+                self.track_progress(1);
                 // self.track_progress(vert_idx, hole_idx);
                 match self.solver.problem.score_vertices(vertices, None) {
                     Ok(score) => (score, Some(problem::Pose {
@@ -62,7 +83,7 @@ impl BruteforceHoleSolver {
                 }
             }
             else {
-                self.run(vert_idx + 1, best_pose_score, vertices, hole)
+                self.run(vert_idx + 1, best_pose_score, vertices, hole, distances)
             };
 
             if new_score == 0 {
@@ -81,10 +102,10 @@ impl BruteforceHoleSolver {
         (best_pose_score, best_pose)
     }
 
-    fn track_progress(&mut self) {
-        let perc_before = ((self.step * 1000) / self.total_steps) as i64;
-        self.step += 1;
-        let perc_after = ((self.step * 1000) / self.total_steps) as i64;
+    fn track_progress(&mut self, step: u128) {
+        let perc_before = ((self.step * 1000) / self.total_steps) as u128;
+        self.step += step;
+        let perc_after = ((self.step * 1000) / self.total_steps) as u128;
 
         if perc_after > perc_before {
             println!("PROGRESS: {} ({} of {})",
