40f8edef1e5678834f3aa1f9115b4fe305c4724e
Gleb Golubitsky <sectoid@gnolltech.org>
2021-07-11 15:10:08 +0300

    SPEC: spec version 4.0 + additional tasks
    This seems to be final version of the spec
    
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- tasks/fetch-problems.sh
+++ tasks/fetch-problems.sh
@@ -2,7 +2,7 @@
 
 API_TOKEN=29a3adf2-b0d3-4166-8891-9c990df11546
 
-for i in `seq 1 106`
+for i in `seq 1 132`
 do
     curl -H "Authorization: Bearer $API_TOKEN" https://poses.live/api/problems/$i -o $i.problem
 done
