af6951bd3b07925d3e91785f117bb4fc423388c4
Gleb Golubitsky <sectoid@gnolltech.org>
2021-07-12 13:08:34 +0300

    run gui as release
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- run-gui.sh
+++ run-gui.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cargo build
-exec ./target/debug/app --border-width 100 --problem-file ./tasks/$1.problem  --pose-file ./poses/$1.pose
+cargo build --release
+exec ./target/release/app --border-width 100 --problem-file ./tasks/$1.problem  --pose-file ./poses/$1.pose
